4881adc5bd565b9e32fbadc20915f35e290cbb8d
Pedro Machado <pedrohsantosmachado@gmail.com>
2025-11-07 11:54:06 -0300

    Principal
    JS adicionado

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ console.log("Olá mundo");
 
 let churrasco = "sim"; 
 
-if ( churrasco == "sim") {
+if ( churrasco == "não") {
     alert("Vai ter churrasco!");
 } else {
     alert ("Então vamos marcar o churrasco");
 }
+        
