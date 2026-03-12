0511c7a2d70872fa2e3cc90df6effd5827fa14b0
Pedro Machado <pedrohsantosmachado@gmail.com>
2026-03-12 19:08:46 -0300

    contador finalizado

--- 01_Contador.de.valores/script.js
+++ 01_Contador.de.valores/script.js
@@ -2,4 +2,29 @@ const valor = document.getElementById('valor');
 const adicionar = document.getElementById('adicionar');
 const subtrair = document.getElementById('subtrair');
 
+const updateValor = () => {
+    valor.innerHTML = count;
+};
+
 let count = 0;
+let intervalID = 0; 
+
+
+adicionar.addEventListener('mousedown', () => {
+    intervalID = setInterval ( () => {
+        count += 1;
+        updateValor();
+    }, 100);
+
+});
+
+subtrair.addEventListener('mousedown', () => {
+    intervalID = setInterval ( () => {
+        count -= 1;
+        updateValor();
+    }, 100);
+
+});
+
+
+document.addEventListener('mouseup', () => clearInterval(intervalID));
