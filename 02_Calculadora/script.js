e433c97f4725e33c2a0a2b6581bb5a8d383ccf43
Pedro Machado <pedrohsantosmachado@gmail.com>
2026-03-17 18:47:24 -0300

    .

--- 02_Calculadora/script.js
+++ 02_Calculadora/script.js
@@ -1,19 +1,74 @@
 const display = document.getElementById("display");
-const buttons = document.querySelectorAll("keys button");
+const botoes = document.querySelectorAll("#keys button");
 
-let current = "";
+let expressao = "";
 
-const updatedisplay = () => {
-    display.innerHTML = count;
-};
+function atualizarDisplay() {
+    display.innerText = expressao || "0";
+}
 
-let count = 0;
-let intervalID = 0; 
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+        let valor = botao.innerText;
 
-display.addEventListener('click', () => {
-    intervalID = setInterval ( () => {
-        count += 1;
-        updatedisplay();
-    }, 100);
+        if (valor === "C") {
+            expressao = "";
+        } 
+        
+        else if (valor === "⌫" || valor === "‹") {
+            expressao = expressao.slice(0, -1);
+        }
+        
+        else if (valor === "=") {
+            try {
+                if (expressao === "") return;
+            let conta = expressao.replace(/x/g, "*");
+            let resultado = eval(conta);
+            expressao = resultado.toString();
+            display.innerText = expressao;
+        } catch (erro) {
+            expressao = "";
+            display.innerText = "Erro";
+        }
+}
+        
+        else {
+            if (valor === "x") {
+                expressao += "*";
+            } else {
+                expressao += valor;
+            }
+        }
 
+        atualizarDisplay();
+    });
+});
+
+
+document.addEventListener("keydown", (e) => {
+    let tecla = e.key;
+
+    if (!isNaN(tecla) || "+-*/.".includes(tecla)) {
+        expressao += tecla;
+    } 
+    
+    else if (tecla === "Enter") {
+        try {
+            expressao = eval(expressao).toString();
+        } catch {
+            expressao = "";
+            display.innerText = "Erro";
+            return;
+        }
+    } 
+    
+    else if (tecla === "Backspace") {
+        expressao = expressao.slice(0, -1);
+    } 
+    
+    else if (tecla.toLowerCase() === "c") {
+        expressao = "";
+    }
+
+    atualizarDisplay();
 });
