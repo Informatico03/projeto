5e75ce9127d0f944b24dfc48d417636d7b420ff2
Pedro Machado <pedrohsantosmachado@gmail.com>
2026-03-17 09:18:15 -0300

    .

--- 02_Calculadora/script.js
+++ 02_Calculadora/script.js
@@ -2,3 +2,18 @@ const display = document.getElementById("display");
 const buttons = document.querySelectorAll("keys button");
 
 let current = "";
+
+const updatedisplay = () => {
+    display.innerHTML = count;
+};
+
+let count = 0;
+let intervalID = 0; 
+
+display.addEventListener('click', () => {
+    intervalID = setInterval ( () => {
+        count += 1;
+        updatedisplay();
+    }, 100);
+
+});
