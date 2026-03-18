99489bb008a6b2da65c21ffc86d4585819bd4d6d
Pedro Machado <pedrohsantosmachado@gmail.com>
2026-03-18 11:02:50 -0300

    Update script.js

---
+++ 04_Lista.de.compra/script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form');
+const input = document.getElementById('item');
+const lista = document.getElementById('lista');
+const contador = document.getElementById('contador');
